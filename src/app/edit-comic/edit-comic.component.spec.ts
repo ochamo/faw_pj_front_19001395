0d56cb7bee4f71b8aaacf7bfa2799054761a895e
Otto Chamo <19001395@galileo.edu>
2022-05-12 18:45:48 -0600

    feature: Create edit comic component

---
+++ src/app/edit-comic/edit-comic.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditComicComponent } from './edit-comic.component';
+
+describe('EditComicComponent', () => {
+  let component: EditComicComponent;
+  let fixture: ComponentFixture<EditComicComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EditComicComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditComicComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/edit-comic/edit-comic.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { CreateComicComponent } from '../create-comic/create-comic.component';
+import { CreateComicModel } from '../shared/model/createComic.model';
+import { EditorialModel } from '../shared/model/editorial.model';
+import { CreateComicService } from '../shared/services/createComic.service';
+import { EditorialService } from '../shared/services/editorial.service';
+
+@Component({
+  selector: 'app-edit-comic',
+  templateUrl: './edit-comic.component.html',
+  styleUrls: ['./edit-comic.component.scss']
+})
+export class EditComicComponent implements OnInit {
+
+  public createComicForm: FormGroup;
+  public editorials: EditorialModel[];
+
+  constructor(
+    private dialogRef: MatDialogRef<CreateComicComponent>,
+    private editorialService: EditorialService,
+    private createComicService: CreateComicService,
+  ) { 
+    this.editorials = [];
+    this.initForm();
+    this.getEditorials();
+  }
+
+  private initForm() {
+    this.createComicForm = new FormGroup({
+      'name': new FormControl('', [Validators.required]),
+      'printYear': new FormControl('', [Validators.maxLength(4)]),
+      'sinopsis': new FormControl('', [Validators.required]),
+      'editorial': new FormControl('', [Validators.required])
+    });
+  }
+
+  ngOnInit(): void {
+    this.editorialService.getEditorials().subscribe({
+      next: (res) => {
+
+      }
+    })
+  }
+
+  private getEditorials() {
+    this.editorialService.getEditorials().subscribe({
+      next: (res) => {
+        this.editorials = res;
+      },
+      error: (error) => {
+      }
+    })
+  }
+
+  public createComic() {
+    if (this.createComicForm.valid) {
+      let comicModel = new CreateComicModel();
+      comicModel.name = this.createComicForm.get('name')?.value;
+      comicModel.printYear = this.createComicForm.get('printYear')?.value;
+      comicModel.sinopsis = this.createComicForm.get('sinopsis')?.value;
+      comicModel.idEditorial = this.createComicForm.get('editorial')?.value;
+
+      this.doCreateComic(comicModel);
+    }
+  }
+
+  private doCreateComic(comic: CreateComicModel) {
+    this.createComicService.createComic(comic).subscribe({
+      next: (res) => {
+        this.dialogRef.close();
+      }, 
+      error: (error) => {
+        console.log(error);
+      }
+    })
+  }
+
+}
